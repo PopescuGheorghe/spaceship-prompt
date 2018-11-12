3445ae286c0adc8a001fe3a27e5ee4a80866bbd0
Gheorghe <gheorghe.popescu@puppet.com>
2018-11-12 15:17:51 -0800

    work with multiple remotes

--- sections/git_branch.zsh
+++ sections/git_branch.zsh
@@ -20,6 +20,7 @@ spaceship_git_branch() {
   [[ $SPACESHIP_GIT_BRANCH_SHOW == false ]] && return
 
   local git_current_branch="$vcs_info_msg_0_"
+  git_remote=${$(git rev-parse --abbrev-ref --symbolic-full-name @{u} |grep -E -o ".{0,20}\/.{0}" 2>/dev/null)}
   [[ -z "$git_current_branch" ]] && return
 
   git_current_branch="${git_current_branch#heads/}"
@@ -27,5 +28,5 @@ spaceship_git_branch() {
 
   spaceship::section \
     "$SPACESHIP_GIT_BRANCH_COLOR" \
-    "$SPACESHIP_GIT_BRANCH_PREFIX${git_current_branch}$SPACESHIP_GIT_BRANCH_SUFFIX"
+    "$SPACESHIP_GIT_BRANCH_PREFIX${git_remote}${git_current_branch}$SPACESHIP_GIT_BRANCH_SUFFIX"
 }
